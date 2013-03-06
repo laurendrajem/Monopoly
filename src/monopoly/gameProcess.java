a23139b1789306791d568c3357f227b3b9eeb002
psychebb <psychebb@163.com>
2013-03-06 23:53:28 +0800

    add main to check roll,query and help

--- src/monopoly/gameProcess.java
+++ src/monopoly/gameProcess.java
@@ -1,16 +1,21 @@
 package monopoly;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class gameProcess {
+public class GameProcess {
     Controller controller;
     List<Player> playerList;
     RichMap map;
     int initMoney;
+    DataInput dataInput;
+    boolean hasRolled=false;
 
-    public gameProcess() {
+    public GameProcess() {
         this.map = new RichMap();
         this.initMoney = 10000;
+        this.dataInput = new DataInput();
+        this.playerList = new ArrayList<Player>();
     }
 
     public void initPlayers(String input) {
@@ -24,19 +29,27 @@ public class gameProcess {
 
     public void action(String command){
         String[] wholeCommand = command.split(" ");
-        String commandPart1 = wholeCommand[0];
-        String commandPart2 = wholeCommand[1];
-
-        if ("roll" == commandPart1){
-            controller.currentPlayer.move(controller.currentPlayer.roll());
+        String commandPart1;
+        String commandPart2 = "";
+        if (wholeCommand.length>1){
+            commandPart1 = wholeCommand[0];
+            commandPart2 = wholeCommand[1];
         }
-        else if ("sell" == commandPart1){
+        else {
+            commandPart1 = wholeCommand[0];
+        }
+
+        if ("roll" .equals(commandPart1) ){
+            controller.currentPlayer.move(controller.currentPlayer.roll());
+            hasRolled=true;
+        }
+        else if ("sell" .equals(commandPart1) ){
             controller.currentPlayer.sellLand(map.getLandByNum(commandPart2.charAt(0) - '0'));
         }
-        else if ("query" == commandPart1) {
+        else if ("query" .equals(commandPart1) ) {
             System.out.println("Query");
         }
-        else if ("help" == commandPart1) {
+        else if ("help".equals(commandPart1) ) {
             System.out.println("Help");
         }
         else
@@ -46,7 +59,7 @@ public class gameProcess {
 
     public void afterMove() {
         if (controller.currentLand.getOwner() != controller.currentPlayer ) {
-            if (controller.currentLand.getOwner() != (Player)null) {
+            if (controller.currentLand.getOwner() == (Player)null) {
                 if (getConfirmation()) {
                     controller.playerBuyLand();
                 }
@@ -60,9 +73,21 @@ public class gameProcess {
                     controller.upgradeLand();
                 }
         }
+        hasRolled = false;
     }
 
     private boolean getConfirmation() {
-        return false;
+        return this.dataInput.getConfirmInfo();
+    }
+
+    public void run(){
+        initPlayers(dataInput.getInputString());
+        while(true){
+            for (int i=0;i<playerList.size();i++){
+                while(hasRolled==false)
+                    action(dataInput.getInputString());
+                afterMove();
+            }
+        }
     }
 }
